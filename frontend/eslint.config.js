5611dd70149f12357f92d4fd24cc41930d6e1e06
Dhanaji3 <dhanajiwaghmode33@gmail.com>
2026-07-21 23:47:29 +0530

    failed jobs check

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,43 +1,54 @@
-import js from '@eslint/js'
-import tseslint from 'typescript-eslint'
-import vuePlugin from 'eslint-plugin-vue'
-import vueParser from 'vue-eslint-parser'
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import vuePlugin from "eslint-plugin-vue";
+import vueParser from "vue-eslint-parser";
 
-export default [
+const config = [
   js.configs.recommended,
   {
-    files: ['**/*.ts', '**/*.vue'],
+    files: ["**/*.ts", "**/*.vue"],
     languageOptions: {
       parser: vueParser,
       parserOptions: {
         parser: tseslint.parser,
-        ecmaVersion: 'latest',
-        sourceType: 'module',
+        ecmaVersion: "latest",
+        sourceType: "module",
       },
       globals: {
-        console: 'readonly',
-        window: 'readonly',
-        document: 'readonly',
-        localStorage: 'readonly',
-        setTimeout: 'readonly',
-        clearTimeout: 'readonly',
-        alert: 'readonly',
+        console: "readonly",
+        window: "readonly",
+        document: "readonly",
+        localStorage: "readonly",
+        sessionStorage: "readonly",
+        setTimeout: "readonly",
+        clearTimeout: "readonly",
+        alert: "readonly",
+        confirm: "readonly",
+        crypto: "readonly",
+        process: "readonly",
+        setInterval: "readonly",
+        clearInterval: "readonly",
       },
     },
     plugins: {
-      '@typescript-eslint': tseslint.plugin,
+      "@typescript-eslint": tseslint.plugin,
       vue: vuePlugin,
     },
     rules: {
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-      '@typescript-eslint/no-explicit-any': 'warn',
-      'vue/multi-word-component-names': 'off',
-      'vue/no-setup-props-destructure': 'off',
-      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/no-explicit-any": "warn",
+      "vue/multi-word-component-names": "off",
+      "vue/no-setup-props-destructure": "off",
+      "no-console": ["warn", { allow: ["warn", "error"] }],
     },
   },
   {
-    ignores: ['**/dist/**', '**/node_modules/**', '**/*.d.ts'],
+    ignores: ["**/dist/**", "**/node_modules/**", "**/*.d.ts"],
   },
-]
+];
+
+export default config;
